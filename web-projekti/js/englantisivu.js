2da22305ff1e024bd32a9d598f2959ee595e754b
leevimmikkonen <leevimmikkonen@gmail.com>
2024-04-05 12:14:29 +0300

    englannin tehtävä proto parannus v2

--- web-projekti/js/englantisivu.js
+++ web-projekti/js/englantisivu.js
@@ -1,14 +1,21 @@
-const input1 = document.querySelector("#word")
-const button = document.querySelector("#button")
-
 function trial(inpt, answer) {
     if (inpt === answer) {
         alert("correct!");
     } else {
-        alert("false");
+        alert("false.");
     }
 }
 
-button.addEventListener('click',() => {
-    const t1 = new trial(input1.value, "word")
-});
+function quess(button, inpt, answer) {
+    button.addEventListener('click',() => {
+        const t = new trial(inpt.value, answer)
+    });
+}
+
+const button1 = document.querySelector("#button1")
+const input1 = document.querySelector("#word1")
+const t1 = new quess(button1, input1, "word")
+
+const button2 = document.querySelector("#button2")
+const input2 = document.querySelector("#word2")
+const t2 = new quess(button2, input2, "potato")

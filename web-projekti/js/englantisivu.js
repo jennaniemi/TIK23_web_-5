a83b8b1f270cdcbf1a014e0396b3c1c3140ff735
leevimmikkonen <leevimmikkonen@gmail.com>
2024-04-18 13:40:54 +0300

    englanti sivun ulkonäköä paranneltu ja kaikki tehtävä kohdat lisätty(ei vielä oikein)

--- web-projekti/js/englantisivu.js
+++ web-projekti/js/englantisivu.js
@@ -1,4 +1,4 @@
-let limit = 4;
+let limit = 20;
 let num = 0;
 let points1 = 'points'
 const full = document.querySelector("#full");
@@ -96,19 +96,84 @@ function pointsCalculate(pointsCurrent) {
 const button1 = document.querySelector("#button1");
 const input1 = document.querySelector("#word1");
 const output1 = document.querySelector("#output1");
-const t1 = guessButton(button1, input1, "word", 2, output1);
+const t1 = guessButton(button1, input1, "word", 5, output1);
 
 const button2 = document.querySelector("#button2");
 const input2 = document.querySelector("#word2");
-const t2 = guessButton(button2, input2, "word", 2, output1);
+const t2 = guessButton(button2, input2, "word", 5, output1);
 
 const button3 = document.querySelector("#button3");
 const input3 = document.querySelector("#word3");
-const output2 = document.querySelector("#output2");
-const t3 = guessButton(button3, input3, "potato", 1, output2);
+const t3 = guessButton(button3, input3, "word", 5, output1);
 
 const button4 = document.querySelector("#button4");
 const input4 = document.querySelector("#word4");
+const t4 = guessButton(button4, input4, "word", 5, output1);
+
+const button5 = document.querySelector("#button5");
+const input5 = document.querySelector("#word5");
+const t5 = guessButton(button5, input5, "word", 5, output1);
+
+const button6 = document.querySelector("#button6");
+const input6 = document.querySelector("#word6");
+const output2 = document.querySelector("#output2");
+const t6 = guessButton(button6, input6, "potato", 5, output2);
+
+const button7 = document.querySelector("#button7");
+const input7 = document.querySelector("#word7");
+const t7 = guessButton(button7, input7, "word", 5, output2);
+
+const button8 = document.querySelector("#button8");
+const input8 = document.querySelector("#word8");
+const t8 = guessButton(button8, input8, "word", 5, output2);
+
+const button9 = document.querySelector("#button9");
+const input9 = document.querySelector("#word9");
+const t9 = guessButton(button9, input9, "word", 5, output2);
+
+const button10 = document.querySelector("#button10");
+const input10 = document.querySelector("#word10");
+const t10 = guessButton(button10, input10, "word", 5, output2);
+
+const button11 = document.querySelector("#button11");
+const input11 = document.querySelector("#word11");
 const output3 = document.querySelector("#output3");
-const t4 = guessButton(button4, input4, "peruna", 1, output3);
+const t11 = guessButton(button11, input11, "peruna", 5, output3);
+
+const button12 = document.querySelector("#button12");
+const input12 = document.querySelector("#word12");
+const t12 = guessButton(button12, input12, "word", 5, output3);
+
+const button13 = document.querySelector("#button13");
+const input13 = document.querySelector("#word13");
+const t13 = guessButton(button13, input13, "word", 5, output3);
+
+const button14 = document.querySelector("#button14");
+const input14 = document.querySelector("#word14");
+const t14 = guessButton(button14, input14, "word", 5, output3);
+
+const button15 = document.querySelector("#button15");
+const input15 = document.querySelector("#word15");
+const t15 = guessButton(button15, input15, "word", 5, output3);
+
+const button16 = document.querySelector("#button16");
+const input16 = document.querySelector("#word16");
+const output4 = document.querySelector("#output4");
+const t16 = guessButton(button16, input16, "potato", 5, output4);
+
+const button17 = document.querySelector("#button17");
+const input17 = document.querySelector("#word17");
+const t17 = guessButton(button17, input17, "word", 5, output4);
+
+const button18 = document.querySelector("#button18");
+const input18 = document.querySelector("#word18");
+const t18 = guessButton(button18, input18, "word", 5, output4);
+
+const button19 = document.querySelector("#button19");
+const input19 = document.querySelector("#word19");
+const t19 = guessButton(button19, input19, "word", 5, output4);
+
+const button20 = document.querySelector("#button20");
+const input20 = document.querySelector("#word20");
+const t20 = guessButton(button20, input20, "word", 5, output4);
 

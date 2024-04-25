71076fa99e7db1ce779778f5ed8740aaee4f1e29
leevimmikkonen <leevimmikkonen@gmail.com>
2024-04-25 09:07:32 +0300

    etusivun pisteiden laskenta muutettu toimivaksi

---
+++ web-projekti/js/index.js
@@ -0,0 +1,52 @@
+const pointsAll = document.querySelector('#pointsall');
+
+pointsAll.innerHTML = 0
+
+let points1 = 'points'
+let points2 = 'pisteet'
+
+//revealing points
+function pointsReveal(pointsSpecific, pointsOutput) {
+    pointsOutput.innerHTML = pointsSpecific
+};
+
+//points retrieve
+function pointsRetrieve(pointsStorage, pointsOutput, num) {
+    let pointsStored = localStorage.getItem(pointsStorage);
+    pointsStored = parseInt(pointsStored);
+    if (isNaN(pointsStored)) {
+        pointsStored = 0;
+    } else {
+        pointsAllCount(pointsStored, pointsOutput);
+    }
+};
+
+//calculating all points
+function pointsAllCount(pointsStored, pointsOutput) {
+    let pointsCount = parseInt(pointsOutput.textContent);
+    pointsCount += pointsStored;
+    pointsReveal(pointsCount, pointsOutput);
+};
+
+function navFunction() {
+    var x = document.getElementById("navigaatio");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
+
+
+pointsRetrieve(points1, pointsAll, 1)
+pointsRetrieve(points2, pointsAll, 1)
+
+
+function navFunction() {
+    var x = document.getElementById("navigaatio");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}

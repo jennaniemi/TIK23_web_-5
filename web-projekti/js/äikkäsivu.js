ae6af839a1d4cf875fe2d5c9a99c9523ff1ce7af
pauluspäivärinta <n3papa00@students.oamk.fi>
2024-05-02 11:59:19 +0300

    Äikkäsivuihin tehty pistelaskenta toimimaan

--- web-projekti/js/äikkäsivu.js
+++ web-projekti/js/äikkäsivu.js
@@ -40,11 +40,16 @@ for (let i = 0; i < tavutusVastaukset.length; i++) {
         console.log(`Odotettu vastaus: "${tavutusVastaukset[i]}"`);
         if (vastaus === tavutusVastaukset[i]) {
             alert('Oikein!');
+            kokonaispisteet++
+            pistemäärä.innerHTML = kokonaispisteet
+            pointsCalculate(kokonaispisteet, pisteet)
         } else {
             alert('Väärin!');
         }
     });
 }
+
+const pistemäärä = document.querySelector("#pistemäärä")
 
 // Sanaluokkatehtävät
 const sanaluokkaVastaukset = [
@@ -69,8 +74,13 @@ for (let i = 0; i < sanaluokkaVastaukset.length; i++) {
         const vastaus = inputElement.value.trim().toLowerCase();
         if (vastaus === sanaluokkaVastaukset[i]) {
             alert('Oikein!');
+            kokonaispisteet++
+            pistemäärä.innerHTML = kokonaispisteet
+            pointsCalculate(kokonaispisteet, pisteet)
+            
         } else {
             alert('Väärin!');
         }
     });
 }
+
